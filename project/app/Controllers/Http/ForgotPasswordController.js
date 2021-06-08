49c079c9b5ee9084d9fcf379ea1215b110654e87
Iago Silva Oliveira <iagooliveira3845@hotmail.com>
2021-06-08 14:18:25 -0300

    feat: Reset password

--- project/app/Controllers/Http/ForgotPasswordController.js
+++ project/app/Controllers/Http/ForgotPasswordController.js
@@ -4,6 +4,7 @@ const Mail = use('Mail')
 const User = use('App/Models/User')
 
 const crypto = require('crypto')
+const moment = require('moment')
 
 class ForgotPasswordController {
   async store ({request, response}) {
@@ -35,6 +36,33 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({error: {message: 'Algo deu errado!'}})
+    }
+  }
+
+  async update ({request, response}) {
+    try {
+      const {token, password} = request.all();
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if(tokenExpired) {
+        return response
+          .status(401)
+          .send({error: {message: 'O token de validação está expirado'}})  
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch(err) {
+      return response
+        .status(err.status)
+        .send({error: {message: 'Algo deu errado ao resetar sua senha!'}})
     }
   }
 }

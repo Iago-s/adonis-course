47e35a9422863dda4ae4eea9a1266e6d6b36b975
Iago Silva Oliveira <iagooliveira3845@hotmail.com>
2021-06-10 09:27:50 -0300

    feat: CreateMany and Transactions

--- project/app/Controllers/Http/UserController.js
+++ project/app/Controllers/Http/UserController.js
@@ -1,13 +1,21 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
   async store({request}) { //Desestruturando request do objeto ctx(context)
     //Filtrando os dados do corpo do request
     const data = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
 
-    const user = await User.create(data);
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx);
+    await user.addresses().createMany(addresses, trx)
+
+    //Se não da nenhum error nas inserções acima ele efetua as mudanças no db
+    await trx.commit()
 
     /*
       Quando passada a flag api-only na criação do projeto o Adonis
